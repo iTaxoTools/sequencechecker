8b488f0683f482ae3781c37468b5300e988a8e5d
Vladimir Kharchev <necrosovereign@gmail.com>
2022-01-31 15:10:22 +0100

    Simple test ui

---
+++ src/itaxotools/sequencechecker/sequencechecker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+from PySide6.QtCore import Qt, QFile, Slot
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+@Slot()
+def say_hello() -> None:
+    print("hello")
+
+
+def main() -> None:
+    app = QApplication(sys.argv)
+    loader = QUiLoader()
+    ui_file = QFile(str(Path(__file__).with_name("sequencechecker.ui")))
+    ui_file.open(QFile.ReadOnly)
+    win = loader.load(ui_file)
+    baz_btn = win.findChild(QPushButton, "baz_btn")
+    baz_btn.clicked.connect(say_hello)
+    ui_file.close()
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
